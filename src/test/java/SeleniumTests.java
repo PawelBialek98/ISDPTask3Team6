cb212cb699be19d7c12645db81f6105578e3f4c2
216866 Plich Jakub <216866@edu.p.lodz.pl>
2020-02-09 22:53:03 +0100

    fix RemoveLocationTest

--- src/test/java/SeleniumTests.java
+++ src/test/java/SeleniumTests.java
@@ -105,50 +105,36 @@ public class SeleniumTests {
         webDriver.quit();
     }
 
-
     @Test
     public void removeLocationTest() {
         webDriver = new FirefoxDriver();
-        webDriver.get("https://localhost.localdomain:8181/faces/main/index.xhtml");
-        webDriver.manage().window().setSize(new Dimension(1920, 886));
-        webDriver.findElement(By.linkText("Sign in")).click();
-        webDriver.findElement(By.name("j_username")).click();
-        webDriver.findElement(By.name("j_username")).click();
-        {
-            WebElement element = webDriver.findElement(By.name("j_username"));
-            Actions builder = new Actions(webDriver);
-            builder.doubleClick(element).perform();
-        }
-        webDriver.findElement(By.name("j_password")).click();
-        webDriver.findElement(By.name("j_password")).click();
-        {
-            WebElement element = webDriver.findElement(By.name("j_password"));
-            Actions builder = new Actions(webDriver);
-            builder.doubleClick(element).perform();
-        }
-        webDriver.findElement(By.name("j_password")).sendKeys("P@ssw0rd");
-        webDriver.findElement(By.cssSelector("input:nth-child(2)")).click();
-        webDriver.findElement(By.linkText("Location")).click();
-        webDriver.findElement(By.linkText("Locations list")).click();
-        webDriver.findElement(By.name("j_idt26:j_idt27:0:onlyWarehouse:j_idt38")).click();
-        int size;
-        {
-            List<WebElement> rows = webDriver.findElements(By.xpath("/html/body/div/div[3]/div/form/table/thead/tr"));
-            size = rows.size();
-            Assert.assertEquals(rows.size(), size);
-        }
+
+        webDriver.navigate().to(url + "faces/main/index.xhtml");
+        webDriver.findElement(By.xpath("/html/body/div/div[2]/div/div/nav/div/div[2]/ul[1]/li[2]/a")).click(); // przejdz do zakladki logowanie
+        webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr/td/table/tbody/tr[1]/td[2]/input")).sendKeys("JDoe");
+        webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr/td/table/tbody/tr[2]/td[2]/input")).sendKeys("P@ssw0rd");
+        webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/p/input")).click();
+
+        webDriver.navigate().to(url + "/faces/location/listLocations.xhtml");
+        List<WebElement> rows = webDriver.findElements(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr"));
+        Assert.assertEquals(rows.size(), 10);
+
+        webDriver.navigate().to(url + "faces/location/createNewLocation.xhtml");
+        webDriver.findElement(By.id("CreateLocationForm:locationSymbol")).sendKeys("XX-12-34-56");
+        Select select = new Select(webDriver.findElement(By.id("CreateLocationForm:locationType")));
+        select.selectByValue("SHELF3");
+        webDriver.findElement(By.name("CreateLocationForm:j_idt34")).click();
+
+        webDriver.navigate().to(url + "/faces/location/listLocations.xhtml");
+        rows = webDriver.findElements(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr"));
+        Assert.assertEquals(rows.size(), 11);
+
+        webDriver.findElement(By.name("j_idt26:j_idt27:10:onlyWarehouse:j_idt38")).click();
         webDriver.findElement(By.name("DeleteLocationForm:j_idt30")).click();
-        {
-            List<WebElement> rows = webDriver.findElements(By.xpath("/html/body/div/div[3]/div/form/table/thead/tr"));
-            if(rows.size() == 0) {
-                Assert.assertEquals(rows.size(), size);
-            }
-            else{
-                Assert.assertEquals(rows.size(), size-1);
-            }
-        }
-        webDriver.findElement(By.linkText("Sign out")).click();
-        webDriver.findElement(By.name("j_idt26:j_idt30")).click();
+
+        rows = webDriver.findElements(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr"));
+        Assert.assertEquals(rows.size(), 10);
+
         webDriver.quit();
     }
 
