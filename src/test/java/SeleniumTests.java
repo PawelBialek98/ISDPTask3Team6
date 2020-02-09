ecd0e4736901d30de1713581a0006b4c5e325bc4
Łukasz Kostrzewa <lukko98@gmail.com>
2020-02-09 19:39:43 +0100

    4.1.2 - Łukasz Kostrzewa - added adding Contractor test

--- src/test/java/SeleniumTests.java
+++ src/test/java/SeleniumTests.java
@@ -1,22 +1,28 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class SeleniumTests {
-    WebDriver webDriver = new FirefoxDriver();
+    WebDriver webDriver;
     private String url = "https://macbook-pro-pawe.local:8181/";
+    //private String url = "https://localhost:8181/";
 
     @BeforeTest
     public void setUp(){
         System.setProperty("webdriver.gecko.driver", "/usr/local/Cellar/geckodriver/0.26.0");
+        //System.setProperty("webdriver.gecko.driver", "C:\\Users\\Lukasz\\Desktop\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+        webDriver = new FirefoxDriver();
     }
 
     @Test
-    void sampleTest(){
+    void modifyLocationTest(){
         webDriver.navigate().to(url + "faces/main/index.xhtml");
         webDriver.findElement(By.xpath("/html/body/div/div[2]/div/div/nav/div/div[2]/ul[1]/li[2]/a")).click(); // przejdz do zakladki logowanie
         webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr/td/table/tbody/tr[1]/td[2]/input")).sendKeys("JDoe");
@@ -41,4 +47,41 @@ public class SeleniumTests {
         Assert.assertEquals(orgValue, finalValue);
         webDriver.quit();
     }
+
+    @Test
+    void addContractorTest(){
+        webDriver.navigate().to(url + "faces/main/index.xhtml");
+        webDriver.findElement(By.xpath("/html/body/div/div[2]/div/div/nav/div/div[2]/ul[1]/li[2]/a")).click(); // przejdz do zakladki logowanie
+        webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr/td/table/tbody/tr[1]/td[2]/input")).sendKeys("LRey");
+        webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr/td/table/tbody/tr[2]/td[2]/input")).sendKeys("P@ssw0rd");
+        webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/p/input")).click(); //click login
+        webDriver.navigate().to(url + "faces/contractor/listContractors.xhtml");
+        List<WebElement> rows = webDriver.findElements(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr"));
+        //System.out.println(rows.size());
+        Assert.assertEquals(rows.size(),4);
+
+        webDriver.navigate().to(url + "faces/contractor/registerContractor.xhtml");
+        webDriver.findElement(By.xpath("//*[@id=\"RegisterContractorForm:contractorNumber\"]")).sendKeys("0048856746851");
+        webDriver.findElement(By.xpath("//*[@id=\"RegisterContractorForm:contractorName\"]")).sendKeys("Jan Kowalski");
+        webDriver.findElement(By.xpath("//*[@id=\"RegisterContractorForm:nip\"]")).sendKeys("542-315-12-58");
+        webDriver.findElement(By.xpath("//*[@id=\"RegisterContractorForm:street\"]")).sendKeys("Wiejska");
+        webDriver.findElement(By.xpath("//*[@id=\"RegisterContractorForm:house\"]")).sendKeys("18");
+        webDriver.findElement(By.xpath("//*[@id=\"RegisterContractorForm:zip\"]")).sendKeys("98-013");
+        webDriver.findElement(By.xpath("//*[@id=\"RegisterContractorForm:city\"]")).sendKeys("Warszawa");
+        webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/input[2]")).click();
+        webDriver.navigate().to(url + "faces/contractor/listContractors.xhtml");
+        rows = webDriver.findElements(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr"));
+        String name = webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr[5]/td[2]")).getText();
+        Assert.assertEquals(name, "Jan Kowalski");
+        Assert.assertEquals(rows.size(),5);
+
+        webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr[5]/td[10]/input[2]")).click();
+        webDriver.findElement(By.xpath("/html/body/div/div[3]/div/form/input[2]")).click();
+        webDriver.navigate().to(url + "faces/contractor/listContractors.xhtml");
+        rows = webDriver.findElements(By.xpath("/html/body/div/div[3]/div/form/table/tbody/tr"));
+        Assert.assertEquals(rows.size(),4);
+
+    }
+
+
 }
